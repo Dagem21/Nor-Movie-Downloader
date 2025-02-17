9945dd569d590d15411c22f7522a4a195a6e6ae8
Dagem21 <dagemmoges23@gmail.com>
2025-02-17 21:45:30 +0300

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,21 @@ import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "./usercontext";
 import { getFavorite } from "@/mongo/favorite";
+import useFetch from "@/hooks/UseFetch";
 
 export default function Home() {
     const { user } = useContext(UserContext)
+
+    const { data, isLoading, fetchData } = useFetch({
+        method: 'GET'
+    })
+    const { data: seasonData, isLoading: seasonLoading, fetchData: seasonFetch } = useFetch({
+        method: 'GET'
+    })
+    const { data: episodeData, isLoading: episodesLoading, fetchData: episodeFetch } = useFetch({
+        method: 'GET'
+    })
+
     const router = useRouter()
     const [movies, setMovies] = useState([])
     const [selectedMovie, setSelectedMovie] = useState()
@@ -20,18 +32,43 @@ export default function Home() {
         call()
     }, [user])
 
+    useEffect(() => {
+        if (data && !isLoading && seasonData && !seasonLoading) {
+            function compare(a, b) {
+                if (a.number < b.number) {
+                    return 1;
+                }
+                if (a.number > b.number) {
+                    return -1;
+                }
+                return 0;
+            }
+            let sortedSeasons = [...seasonData]
+            sortedSeasons.sort(compare);
+            setSelectedMovie({ data, seasons: sortedSeasons })
+        }
+    }, [data, isLoading, seasonData, seasonLoading])
+
+    console.log(selectedMovie?.seasons);
+
+    const handleSelected = (movieID) => {
+        fetchData({ url: `https://api.tvmaze.com/shows/${movieID}` })
+        seasonFetch({ url: `https://api.tvmaze.com/shows/${movieID}/seasons` })
+    }
+    console.log();
+
     return (
         <div className="h-screen">
             <Header />
             <div className="grid grid-cols-4">
-                <div className="col-start-1 col-span-1 border border-solid shadow-xl rounded-xl border-stone-600 ml-3 p-3">
+                <div className="col-start-1 col-span-1 border border-solid shadow-xl rounded-xl border-stone-600 ml-3 p-3 h-fit">
                     <h1 className="text-l font-bold text-gray-300 text-center mb-3">Favorite</h1>
                     {
                         movies.map((movie) => {
                             return <h1
                                 key={movie?.movieID}
-                                className={`py-1 text-sm font-bold ${selectedMovie === movie ? 'border-r border-solid border-indigo-600' : ''}`}
-                                onClick={() => { setSelectedMovie(movie) }}
+                                className={`py-1 text-sm font-bold ${selectedMovie?.data?.id + '' === movie?.movieID ? 'border-r border-solid border-indigo-600' : ''}`}
+                                onClick={() => { handleSelected(movie?.movieID) }}
                             >
                                 {movie.movieName}
                             </h1>
@@ -53,8 +90,50 @@ export default function Home() {
                 <div className="col-start-2 col-span-3 flex justify-center items-center">
                     {
                         selectedMovie ?
-                            <div>
-
+                            <div className="col-start-2 col-span-3 px-10 py-3 flex">
+                                <div className="flex justify-center mr-4">
+                                    <img className="max-w-64" src={selectedMovie?.data?.image?.original} />
+                                </div>
+                                <div>
+                                    <div className="flex justify-between">
+                                        <h1 className="text-2xl font-bold text-gray-300 text-start">{selectedMovie?.data?.name}</h1>
+                                        <button
+                                            type="button"
+                                            className="text-black bg-red-500 hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2"
+                                            onClick={() => { }}
+                                        >
+                                            <svg className="mr-2" width="20px" height="20px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M4 7H20" stroke="#000000" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round" />
+                                                <path d="M6 7V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V7" stroke="#000000" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round" />
+                                                <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#000000" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round" />
+                                            </svg>
+                                            Remove
+                                        </button>
+                                    </div>
+                                    <div className="flex justify-between pt-5">
+                                        <h1 className="text-l font-bold text-blue-300 text-center"><span className="text-sm font-bold text-gray-300">Rating: </span>{selectedMovie?.data?.rating?.average}</h1>
+                                        <h1 className="text-l font-bold text-blue-300 text-center"><span className="text-sm font-bold text-gray-300">Status: </span>{selectedMovie?.data?.status}</h1>
+                                        <h1 className="text-l font-bold text-blue-300 text-center"><span className="text-sm font-bold text-gray-300">Genres: </span>{selectedMovie?.data?.genres?.join(', ')}</h1>
+                                    </div>
+                                    <div className="flex justify-between pt-5">
+                                        <h1 className="text-l font-bold text-blue-300 text-center"><span className="text-sm font-bold text-gray-300">Premiered:  </span>{selectedMovie?.data?.premiered ? new Date(selectedMovie?.data?.premiered).toDateString() : 'N/A'}</h1>
+                                    </div>
+                                    <div className="flex justify-between pt-5">
+                                        <h1 className="text-l font-bold text-blue-300 text-start"><span className="text-sm font-bold text-gray-300">Summary:  </span>{selectedMovie?.data?.summary?.replace(/(<([^>]+)>)/gi, "")}</h1>
+                                    </div>
+                                    {
+                                        selectedMovie?.seasons &&
+                                        <div className="mt-5 overflow-x-scroll bg-red-500">
+                                            <div className="flex">
+                                                {selectedMovie.seasons.map((season) => {
+                                                    return <button className="text-nowrap mr-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                                        Season {season?.number}
+                                                    </button>
+                                                })}
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
                             </div>
                             :
                             <h1 className="text-xl font-bold">Select a movie!</h1>
--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -1,36 +1,121 @@
-import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useEffect, useState, useCallback } from 'react';
+const useFetch = (
+    requestConfigParam = {
+        url: '',
+        method: '',
+        data: {},
+        params: {},
+        headers: {},
+        auth: {},
+    }, intialFetch = false) => {
 
-const useFetch = ({ header, body="GET", url, method }) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [errors, setErrors] = useState({});
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [fetchError, setFetchError] = useState(null);
+    const [requestConfig, setRequestConfig] = useState(requestConfigParam);
 
-    const fetch = async () => {
+    const fetchData = useCallback(async (request) => {
+        setIsLoading(true);
+        setErrors({});
         try {
-            console.log(url);
-            // setLoading(true)
-            const response = await fetch(
-                url,
-                {
-                    method: method,
-                    header: header,
-                    body: body,
-                }
-            )
-            if (response.ok) {
-                const json = await response.json()
-                // setData(json)
-                console.log(json);
+            const response = await axios.request({
+                ...requestConfig,
+                ...request,
+            });
+            setData(response.data || []);
+
+        }
+        catch (error) {
+            if (error?.response?.status === 400) {
+                setErrors({
+                    isError: true,
+                    message: 'Bad request',
+                    details: error
+                });
+            }
+            else if (error?.response?.status === 401) {
+                setErrors({
+                    isError: true,
+                    message: 'Unauthorized',
+                    details: error
+                });
+            }
+            else if (error?.response?.status === 404) {
+                setErrors({
+                    isError: true,
+                    message: 'NOT FOUND',
+                    details: error
+                });
+
+            }
+            else if (error?.response?.status === 422) {
+                setErrors({
+                    isError: true,
+                    message: 'Invalid Data, check your inputs',
+                    details: error,
+                });
+            }
+            else if (error?.response?.status === 415) {
+                setErrors({
+                    isError: true,
+                    message: 'Invalid MIME type, check your inputs',
+                    details: error,
+                });
+
+            }
+            else if (error?.response?.status && 400 <= error?.response?.status && error?.response?.status <= 499) {
+                setErrors({
+                    isError: true,
+                    message: 'Bad Request',
+                    details: error
+                });
+            }
+            else if (error?.response?.status && 500 <= error?.response?.status && error?.response?.status <= 599) {
+                setErrors({
+                    isError: true,
+                    message: 'Server Error',
+                    details: error
+                });
+            }
+            else if (error?.response?.data?.message) {
+                setErrors({
+                    isError: true,
+                    message: error?.response?.message,
+                    details: error,
+                });
+
+            }
+            else if (error?.message) {
+                setErrors({
+                    isError: true,
+                    message: error?.message,
+                    details: error
+                });
+
+            }
+            else {
+                setErrors({
+                    isError: true,
+                    message: 'Unknown Error'
+                });
             }
         }
-        catch (e) {
-            // setFetchError(e)
-        }
         finally {
-            // setLoading(false)
+            setIsLoading(false);
         }
-    }
-    return ({ fetch:fetch, data, loading, fetchError })
+    }, []);
+
+    useEffect(() => {
+        intialFetch && fetchData(requestConfig);
+    }, [requestConfig, fetchData, intialFetch]);
+
+    return {
+        isLoading, errors,
+        data, setData,
+        fetchData, setErrors
+    };
 };
+
 
 export default useFetch;
